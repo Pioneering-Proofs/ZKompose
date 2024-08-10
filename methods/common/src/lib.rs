eaa92a4d320038fdcc9732cfcd58538ae8311142
Kai Aldag <kaialdag@icloud.com>
2024-08-10 04:17:15 -0400

    Feed team input json to circuit

--- methods/common/src/lib.rs
+++ methods/common/src/lib.rs
@@ -1,1 +1,7 @@
 //! Shared types and utility functions between the host and guest.
+
+/// Types used across the project
+pub mod types;
+
+/// Utilities and miscellaneous implementations
+pub mod utils;
--- methods/guest/src/bin/build_team.rs
+++ methods/guest/src/bin/build_team.rs
@@ -1,9 +1,25 @@
 use std::io::Read;
 
-use alloy_primitives::U256;
-use alloy_sol_types::SolValue;
+use alloy_primitives::{address, Address, U256};
+use alloy_sol_types::{sol, SolValue};
+use json::parse;
 use risc0_zkvm::guest::env;
 
+use common::types::{Player, PlayerData, Skills, Team};
+
+/// Alloy interface to use for verifying on-chain state
+sol! {
+    interface IERC721 {
+        function tokenURI(uint256 tokenId) external view returns (string memory);
+        function ownerOf(uint256 tokenId) external view returns (address owner);
+    }
+}
+
 fn main() {
+    // TODO: Signature of owner will need to be provided to ensure this operation is authorized
+    let data: String = env::read();
+    let data = parse(&data).unwrap();
+    println!("Team name: {}", data["name"].as_str().unwrap());
+
     env::commit_slice(U256::from(42).abi_encode().as_slice());
 }
