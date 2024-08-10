d844010079f66c9e41184ad06b342c51bd78be7f
Kai Aldag <kaialdag@icloud.com>
2024-08-10 14:14:38 -0400

    Request pack contract changes

--- contracts/interfaces/IPlayers.sol
+++ contracts/interfaces/IPlayers.sol
@@ -17,4 +17,51 @@ interface IPlayers is IERC721 {
     /// @notice Reverted if a given token has already been minted
     error ERC721AlreadyMinted(uint256 tokenId);
 
+    /// @notice Reverted if a player does not have enough funds to mint a pack
+    error InsufficientFunds(uint256 cost, uint256 paid);
+
+    /// @notice Reverted if a player requests a pack with an invalid tier
+    error InvalidTier();
+
+    //  ─────────────────────────────────────────────────────────────────────────────
+    //  Types
+    //  ─────────────────────────────────────────────────────────────────────────────
+
+    /// @notice Represents a player's rarity
+    enum Tier {
+        Diamond,
+        Platinum,
+        Gold,
+        Silver,
+        Bronze
+    }
+
+    /// @notice Simple eliptic curve point
+    struct Secp256k1PubKey {
+        uint256 x;
+        uint256 y;
+    }
+
+    //  ─────────────────────────────────────────────────────────────────────────────
+    //  Events
+    //  ─────────────────────────────────────────────────────────────────────────────
+
+    event PackRequested(
+        address indexed requester, uint256 indexed packId, Tier indexed tier, Secp256k1PubKey key
+    );
+
+    //  ─────────────────────────────────────────────────────────────────────────────
+    //  Minting Functions
+    //  ─────────────────────────────────────────────────────────────────────────────
+
+    /**
+     * @notice Requests a pack be minted
+     */
+    function requestPack(Tier tier, Secp256k1PubKey calldata key) external payable returns (uint256 packId);
+
+    /**
+     * @notice Returns the cost of a pack
+     */
+    function costOfPack(Tier tier) external pure returns (uint256 cost);
+
 }
