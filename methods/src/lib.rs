1f247bfe04dfa1956eadb5988a3a0abf7207c1bd
DeveloperTheExplorer <arvin.ansari68@gmail.com>
2024-08-10 16:36:22 -0400

    chore: player gen dev complete

--- methods/src/lib.rs
+++ methods/src/lib.rs
@@ -17,47 +17,74 @@ include!(concat!(env!("OUT_DIR"), "/methods.rs"));
 
 #[cfg(test)]
 mod tests {
-    use alloy_primitives::{address, Address, U256};
+    use alloy_primitives::{address, Address, U256, U8};
     use alloy_sol_types::SolValue;
-    use common::types::{Player, PlayerData, PlayerPosition, Skills, Team};
+    use common::types::{GenPlayersInput, Player, PlayerData, PlayerPosition, Skills, Team};
     use json::{parse, stringify};
     use risc0_zkvm::{default_executor, guest::env::write_slice, serde, ExecutorEnv};
     use std::{env::current_dir, fs};
 
     #[test]
-    fn prove_build_team() {
-        let input_data = include_str!("../../data/teams/0.json");
-        // println!("Input data: {}", input_data);
-        let mut input_data = parse(input_data).unwrap();
-        for n in 0..10 {
-            let current = current_dir().unwrap();
-            let file_name: String;
-            if current.ends_with("methods") {
-                file_name = format!("../data/players/{}.json", n);
-            } else {
-                file_name = format!("../../data/players/{}.json", n);
-            }
-            println!(
-                "Reading player data from: {} from {}",
-                file_name,
-                current.display()
-            );
-            let player_data =
-                fs::read_to_string(file_name).expect("Should have been able to read the file");
-            input_data["players"][n] = parse(&player_data).unwrap();
-        }
-        println!("Running build team. Data length: {}", input_data.len());
-        // println!("Input data: {}", input_data.to_string());
+    // fn prove_build_team() {
+    //     let input_data = include_str!("../../data/teams/0.json");
+    //     // println!("Input data: {}", input_data);
+    //     let mut input_data = parse(input_data).unwrap();
+    //     for n in 0..10 {
+    //         let current = current_dir().unwrap();
+    //         let file_name: String;
+    //         if current.ends_with("methods") {
+    //             file_name = format!("../data/players/{}.json", n);
+    //         } else {
+    //             file_name = format!("../../data/players/{}.json", n);
+    //         }
+    //         println!(
+    //             "Reading player data from: {} from {}",
+    //             file_name,
+    //             current.display()
+    //         );
+    //         let player_data =
+    //             fs::read_to_string(file_name).expect("Should have been able to read the file");
+    //         input_data["players"][n] = parse(&player_data).unwrap();
+    //     }
+    //     println!("Running build team. Data length: {}", input_data.len());
+    //     // println!("Input data: {}", input_data.to_string());
+
+    //     let env = ExecutorEnv::builder()
+    //         .write(&input_data.to_string())
+    //         .unwrap()
+    //         .build()
+    //         .unwrap();
+
+    //     let session_info = default_executor()
+    //         .execute(env, super::BUILD_TEAM_ELF)
+    //         .unwrap();
+    // }
+    #[test]
+    fn prove_gen_players() {
+        // let player_count = U8::from(10);
+        // let std_dev = U8::from(10);
+        // let median = U8::from(50);
+        let input = GenPlayersInput {
+            player_count: 10,
+            std_dev: 10,
+            median: 50,
+            u: 3.14159,
+            v: 2123.71828,
+        };
 
         let env = ExecutorEnv::builder()
-            .write(&input_data.to_string())
-            .unwrap()
+            .write(&input)
+            .expect("Invalid input")
             .build()
             .unwrap();
 
         let session_info = default_executor()
-            .execute(env, super::BUILD_TEAM_ELF)
+            .execute(env, super::GEN_PLAYER_ELF)
             .unwrap();
+
+        let players: Vec<PlayerData> = serde::from_slice(&session_info.journal.bytes).unwrap();
+
+        println!("Session info: {:?}", players);
     }
 
     #[test]
