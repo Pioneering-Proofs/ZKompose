9e0902ab9c7accdc4a6a87ea199384234f44fb42
arian233 <esfahanarian@gmail.com>
2024-08-11 01:09:31 -0700

    chore: Add @radix-ui/react-dialog and @radix-ui/react-slot dependencies, update title and description in RootLayout, and add Navbar component in RootLayout

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -4,12 +4,14 @@ import { useAccount, useConnect, useDisconnect } from "wagmi";
 import Navbar from "../components/Navbar";
 import Cards from "@/components/Cards";
 import { UserProvider } from "./UserProvider";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const { address, status, chainId } = useAccount();
   const { connectors, connect, error } = useConnect();
   const { disconnect } = useDisconnect();
 
+  const account = useAccount();
   return (
     <UserProvider>
       {/* <div>
@@ -44,8 +46,23 @@ function App() {
         <div>{status}</div>
         <div>{error?.message}</div>
       </div> */}
+      <div className="ml-auto flex gap-2">
+        {account.status === "connected" && (
+          <Button variant={"outline"} onClick={() => disconnect()}>
+            Disconnect
+          </Button>
+        )}
+        {connectors.map((connector) => (
+          <Button
+            key={connector.uid}
+            onClick={() => connect({ connector })}
+            type="button"
+          >
+            {connector.name}
+          </Button>
+        ))}
+      </div>
 
-      <Navbar />
       <Cards address={address} />
     </UserProvider>
   );
--- next/src/app/providers.tsx
+++ next/src/app/providers.tsx
@@ -1,17 +1,18 @@
-'use client'
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { type ReactNode, useState } from 'react'
-import { type State, WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type ReactNode, useState } from "react";
+import { type State, WagmiProvider } from "wagmi";
+import "./styles/globals.css";
 
-import { getConfig } from '@/wagmi'
+import { getConfig } from "@/wagmi";
 
 export function Providers(props: {
-  children: ReactNode
-  initialState?: State
+  children: ReactNode;
+  initialState?: State;
 }) {
-  const [config] = useState(() => getConfig())
-  const [queryClient] = useState(() => new QueryClient())
+  const [config] = useState(() => getConfig());
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <WagmiProvider config={config} initialState={props.initialState}>
@@ -19,5 +20,5 @@ export function Providers(props: {
         {props.children}
       </QueryClientProvider>
     </WagmiProvider>
-  )
+  );
 }
