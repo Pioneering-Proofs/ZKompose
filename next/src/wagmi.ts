2eb5e5dd40ca2ab248c0d246542a9d56a574ea01
DeveloperTheExplorer <arvin.ansari68@gmail.com>
2024-08-11 04:11:38 -0400

    chore: remove mainnet

--- next/src/wagmi.ts
+++ next/src/wagmi.ts
@@ -1,21 +1,19 @@
 import { http, cookieStorage, createConfig, createStorage } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
-import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
+import { sepolia } from 'wagmi/chains'
+import { coinbaseWallet, injected } from 'wagmi/connectors'
 
 export function getConfig() {
   return createConfig({
-    chains: [mainnet, sepolia],
+    chains: [sepolia],
     connectors: [
       injected(),
       coinbaseWallet(),
-      walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID }),
     ],
     storage: createStorage({
       storage: cookieStorage,
     }),
     ssr: true,
     transports: {
-      [mainnet.id]: http(),
       [sepolia.id]: http(),
     },
   })
